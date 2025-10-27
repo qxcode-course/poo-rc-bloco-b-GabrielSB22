27fc10dd9dd946193297c63425dbedfcdd8dfb5b
GabrielSB22 <gabrielkachan8@gmail.com>
2025-10-27 23:02:46 +0000

    motoca finalizada dps de tempos

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -1,94 +1,94 @@
 class Pessoa:
-    def __init__(self, name: str, age: int):
-        self.__name = name
-        self.__age = age
-        
+    def __init__(self, nome : str, age: int):
+        self.__name = nome 
+        self.__age = age 
+
     def getAge(self):
         return self.__age
-
     def getName(self):
-        return self.__name
-
+        return self.__name 
     def __str__(self):
-        return f"{getName}:{getAge}"
-
+        return f"{self.__name}:{self.__age}"
+    
 class Motoca:
-    def __init__(self, potencia: int, time:int, pessoa):
-        self.__potencia = 1
-        self.__time = 0
-        self.__pessoa = None
+    def __init__(self, potencia : int = 1 ):
+        self.__potencia = potencia
+        self.__time : int = 0
+        self.__pessoa = None 
 
     def __str__(self):
-        return f"power:{self.__potencia}, time:{self.__time}, person:({self.__pessoa})"
+        if self.__pessoa is not None:
+            return f"power:{self.__potencia}, time:{self.__time}, person:({self.__pessoa})"
+        else:
+            return f"power:{self.__potencia}, time:{self.__time}, person:(empty)"
 
-    def Inserir(self, pessoa: Pessoa) -> bool:
+    def inserir(self, pessoa: Pessoa) -> bool:
         if self.__pessoa is not None:
             print("fail: busy motorcycle")
-        self.__pessoa = Pessoa
-        return
-
-    def Remover(self):
+            return False
+        self.__pessoa = pessoa 
+        return True 
+    
+    def remover(self):
         if self.__pessoa is None:
             print("fail: empty motorcycle")
-            return None
-        pessoaremovida = self.__pessoa
+            return None 
+        pessoa_removida = self.__pessoa
         self.__pessoa = None
-        return pessoaremovida
-
-    def BuyTime(self, time: int):
-         self.__time += time
-
-    def Drive(self, time: int):
+        return pessoa_removida
+    
+    def buyTime(self, time: int):
+        self.__time += time 
+    
+    def drive(self, timer: int ):
         if self.__time == 0:
             print("fail: buy time first")
-            return
-        elif self.__pessoa is None:
-            print("fail: empty motorcicle")
-            return
-        elif self.__age > 10:
+            return 
+        if self.__pessoa is None:
+            print("fail: empty motorcycle")
+            return 
+        if self.__pessoa.getAge() > 10:
             print("fail: too old to drive")
             return
-        elif time > self.__time:
-            print("fail: time finished after", self.__time, "minutes")
-            self.__time = 0
-            return
+        if timer > self.__time : 
+            print(f"fail: time finished after {self.__time} minutes")
+            self.__time = 0 
         else:
-            self.__time -= time
-            return
+            self.__time -= timer
 
-    def Init(self):
-        self.__potencia = 1
-        self.__time = 0
-        self.__pessoa = None
-
-    def Honk(self):
-        print("P" + "e" * self.__potencia + "m!")
-
-
+    def honk(self):
+        return "P" + ("e" * self.__potencia) + "m"
+    
 def main():
-    motinha = Motoca()
+    moto = Motoca()
 
     while True:
         line = input()
         print("$" + line)
         args = line.split()
-    
-    if args[0] == "end":
-        break
-    elif args[0] == "show":
-        print(motinha)
-    elif args[0] == "init":
-        motinha.Init()
-    elif args[0] == "enter":
-        nome = args[1]
-        idade = int(args[2])
-        motinha.Inserir(nome, idade)
-    elif args[0] == "leave":
-        motinha.leave()
-    elif args[0] == "buy":
-        motinha.BuyTime(int(args[1]))
-    elif args[0] == "honk":
-        motinha.Honk()
-    else:
-        print("fail: comando inválido")
-main()     
+
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(moto)
+        elif args[0] == "init":
+            potencia = int(args[1])
+            moto = Motoca(potencia) 
+        elif args[0] == "enter":
+            name = args[1]
+            idade = int(args[2])
+            pessoa = Pessoa(name, idade)
+            moto.inserir(pessoa)
+        elif args[0] == "leave":
+            pessoa  = moto.remover()
+            if pessoa is not None:
+                print(pessoa)
+        elif args[0] == "buy":
+            moto.buyTime(int(args[1]))
+        elif args[0] == "drive":
+            time = int(args[1])
+            moto.drive(time)
+        elif args[0] == "honk":
+            print(moto.honk())
+
+main()
